a1dc3803713898edeb38fbd5c1bc195e888a394a
TimurQQ <ilyasovtimur2001@gmail.com>
2025-07-22 02:58:13 +0300

    Fix domain model android dependency Move resources to resources module Update lib versions Remove redundant imports Update gradle version Fix gitignore file Remove deprecated and unstable features and syntax

--- app/src/main/java/com/kernelescape/compose/petmatch/ui/theme/Theme.kt
+++ app/src/main/java/com/kernelescape/compose/petmatch/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.kernelescape.compose.petmatch.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,19 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+// Конфигурация репозиториев для всех проектов
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// Конфигурация репозиториев для buildscript
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
